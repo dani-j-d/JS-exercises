d27950ecd1f5b3b88fad7a5bd8745b0315b427ab
Danni Douglas <dani.j.douglas@gmail.com>
2024-07-11 10:51:57 +0100

    feat(ch-4): add reverseArrayInPlace functionality

--- ch-4-exercises/ex-reverse-array.js
+++ ch-4-exercises/ex-reverse-array.js
@@ -15,3 +15,17 @@ const reverseArray = (array) => {
     return newArray;
 }
 
+const reverseArrayInPlace = (array) => {
+    
+    // input check
+    if (!array.length) {
+        return;
+    }
+    
+    // remove the last element and splice it into current index location
+    for (let index = 0; index < array.length; index++) {
+        array.splice(index, 0, array.pop());
+    }
+
+    return array;
+}
