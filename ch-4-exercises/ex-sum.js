a2bcfda968e9cbefaf3beea2dabbdd9fd0d6080f
Danni Douglas <dani.j.douglas@gmail.com>
2024-07-11 05:27:12 +0100

    feat(ch-4): modify range function to accept either positive or negative steps

--- ch-4-exercises/ex-sum.js
+++ ch-4-exercises/ex-sum.js
@@ -1,16 +1,36 @@
 
-const range = (start, end) => {
+const range = (start, end, step = 1) => {
 
     let numberRange = [];
+    const stepDirection = Math.sign(step);
 
     // input sanitization
-    if( typeof start !== "number" || typeof end !== "number" ) {
+    if( 
+        typeof start !== "number" ||
+        typeof end !== "number" ||
+        (start < end && stepDirection === -1) || // 'end' value must be lower than 'start' in order to count down
+        (start > end && stepDirection === 1)
+    ) {
+        console.log("Invalid parameters")
         return;
     }
 
-    // starting with the first parameter, add the value to an array and count up until reaching the second parameter
-    for (let index = start; index <= end; index++) {
-        numberRange.push(index);
+    if (stepDirection === 1) {
+
+        // starting with the first parameter, add the value to an array and count up until reaching the second parameter
+        for (let index = start; index <= end ; index = index + step) {
+            numberRange.push(index);
+        }
+    } else if (stepDirection === -1) {
+
+        // starting with the first parameter, add the value to an array and count down until reaching the second parameter
+        for (let index = start; index >= end ; index = index + step) {
+            numberRange.push(index);
+        }
+    } else if (stepDirection === 0) {
+
+        // if the user specifies a step of zero, then only the start parameter is added
+        numberRange.push(start);
     }
 
     return numberRange;
