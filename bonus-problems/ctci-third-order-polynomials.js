10e24c28db7e6f87398a86a7a4ec6c81e41c4730
Danni Douglas <dani.j.douglas@gmail.com>
2024-07-29 09:44:08 +0100

    feat(bonus-problems): refined solution to use a map of lists and managed to adjust conditions to provide unique solutions only

--- bonus-problems/ctci-third-order-polynomials.js
+++ bonus-problems/ctci-third-order-polynomials.js
@@ -1,26 +1,45 @@
 
 // find all positive integer solutions such that a^3 + b^3 = c^3 + d^3
 
-let pairs = new Map();
-let solutions = new Map();
-let n = 100;
-for (let i = 1; i < n; i++) {
-    for (let j = 1; j < n; j++) {
-        let result = i**3 + j**3;
-        pairs.set([i, j], result);
+let n = 1000;
+let pairMap = new Map();
+let solutions = [];
+
+// build a map of pairs (value) and the sum of the cubes of the pairs (key)
+for (let c = 1; c <= n; c++) {
+    for (let d = 1; d <= n; d++) {
+        if (c !== d) {
+            let list;
+            let result = c**3 + d**3;
+            let pair = [c,d];
+            // check for an existing list of pairs for the current sum of cubes
+            pairMap.has(result) ? list = pairMap.get(result) : list = [];
+            // prevent duplicate pairs, even if they are not in the same order
+            if (
+                !list.find((existingPair) => existingPair.toString() === pair.toString()) &&
+                !list.find((existingPair) => existingPair.toString() === pair.reverse().toString())
+            ) {
+                list.push(pair);
+            }
+            pairMap.set(result, list);
+        }
     }
 }
-console.log(pairs)
-pairs.forEach((result, pair) => {
-    pairs.forEach((anotherResult, anotherPair) => {
-        if (
-            anotherResult === result &&
-            solutions.has(pair) === false &&
-            anotherPair !== pair
+
+// check through all pairs for unique matches and add to solutions
+pairMap.forEach((pairList, result) => {
+    pairList.forEach((pair) => {
+        pairList.forEach((pair2) => {
+            if (
+                pair.toString() !== pair2.toString() &&
+                pair.toString() !== pair2.reverse().toString() &&
+                !solutions.find(solution => solution.toString() === [...pair,...pair2].toString()) &&
+                !solutions.find(solution => solution.sort().toString() === [...pair,...pair2].sort().toString())
             ) {
-            solutions.set(pair, anotherPair);
-        }
+                solutions.push([...pair,...pair2]);
+            }
+        })
     })
 })
-
 console.log(solutions);
+
